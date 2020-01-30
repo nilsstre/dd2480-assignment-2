f4b05965d1796b4bdd1bdf7d1843fcb1e12eff69
Nils Streijffert <nils.streijffert@gmail.com>
2020-01-30 16:13:06 +0100

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,11 @@
-import org.apache.log4j.BasicConfigurator;
 import utilities.ScriptRunner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            if (new ScriptRunner().cloneRepository()) {
-                if (new ScriptRunner().testProject()) {
-                    if (new ScriptRunner().cleanUpProject()) {
+            if (new ScriptRunner("projectId", "master").cloneRepository(15000, false)) {
+                if (new ScriptRunner("projectId", "master").testProject(15000, false)) {
+                    if (new ScriptRunner("projectId", "master").cleanUpProject(15000, false)) {
                         System.out.println("Finished");
                     } else {
                         System.err.println("Failed to clean up the project");
