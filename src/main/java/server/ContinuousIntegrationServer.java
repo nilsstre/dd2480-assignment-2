44ea9ec68d19d3d1486cd516ee981eff2c8dc428
Nils Streijffert <nils.streijffert@gmail.com>
2020-01-30 14:16:24 +0100

    Add server

---
+++ src/main/java/server/ContinuousIntegrationServer.java
@@ -0,0 +1,55 @@
+package server;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import org.json.*;
+
+/**
+ Skeleton of a ContinuousIntegrationServer which acts as webhook
+ See the Jetty documentation for API documentation of those classes.
+ */
+public class ContinuousIntegrationServer extends AbstractHandler {
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            String payload = IOUtils.toString(request.getReader());
+            try {
+                JSONObject jsonObject = new JSONObject(payload);
+                Iterator<String> iterator = jsonObject.keys();
+                while (iterator.hasNext()) {
+                    String key = iterator.next();
+                    System.out.println(key + ": " + jsonObject.get(key));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        response.getWriter().println("CI job done");
+    }
+
+    // used to start the CI server in command line
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new ContinuousIntegrationServer());
+        server.start();
+        server.join();
+    }
+}
