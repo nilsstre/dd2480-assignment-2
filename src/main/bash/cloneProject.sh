b55d09a5d83519cc23a32ca5f64e363a3aa0b3cd
Nils Streijffert <nils.streijffert@gmail.com>
2020-01-30 16:39:42 +0100

    Update path

--- src/main/bash/cloneProject.sh
+++ src/main/bash/cloneProject.sh
@@ -3,8 +3,8 @@ echo "$1"
 mkdir "$1"
 cd "$1" || exit 1
 git clone git@github.com:nilsstre/dd2480-assignment-2.git
-if [ "$2" != "master" ];
+if [ "$3" != "master" ];
 then
-  cd dd2480-assignment-2 || 1
-  git checkout "$2"
+  cd "$2" || 1
+  git checkout "$3"
 fi
--- src/main/bash/runTests.sh
+++ src/main/bash/runTests.sh
@@ -1,2 +1,2 @@
-cd src/main/resources/git/"$1"/dd2480-assignment-2 || exit 1
+cd src/main/resources/git/"$1"/"$2" || exit 1
 mvn clean install > ../../reports/"$1"
